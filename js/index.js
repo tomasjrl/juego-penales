1d1eae5b54f44865383598e8af341f7b3b29719e
tomasjrl <tstabilini@gmail.com>
2024-08-09 19:40:00 -0300

    reasigno variables

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ import {getComputadoraOpciones , condicionGanador } from "./funciones.js";
 export let puntajes = {
     jugadorPuntaje: 0,
     computadoraPuntaje: 0,
+    penalNumero: 0,
     pateadorTurno: true
 };
 
-export let penalNumero = 0;
 export let ComputadoraEleccion;
 export let jugadorEleccion;
 
@@ -23,16 +23,15 @@ document.querySelectorAll('.opcion').forEach(button => {
 
 
 function playGame(jugadorEleccion) {
-    penalNumero++; 
-    document.getElementById('tanda-de-penales').innerText = penalNumero; // Imprimir el número de la ronda
-    console.log(penalNumero);
+    puntajes.penalNumero++; 
+    console.log(puntajes.penalNumero);
 
     ComputadoraEleccion = getComputadoraOpciones();
     const result = condicionGanador(jugadorEleccion, ComputadoraEleccion, puntajes);
 
 
 
-    const casillero = document.getElementById(penalNumero); // Obtener la casilla correspondiente al turno actual
+    const casillero = document.getElementById(puntajes.penalNumero); // Obtener la casilla correspondiente al turno actual
     casillero.innerHTML = `<img src="${jugadorEleccion !== ComputadoraEleccion ? 'assets/marcador/convertido.svg' : 'assets/marcador/fallado.svg'}">`;
         
     // Actualizar el marcador
@@ -41,7 +40,7 @@ function playGame(jugadorEleccion) {
 
 
 
-    // Cambiar el penalNumero
+    // Cambiar el puntajes.penalNumero
     puntajes.pateadorTurno = !puntajes.pateadorTurno;
 
 
@@ -49,7 +48,7 @@ function playGame(jugadorEleccion) {
 
 
 
-    if (penalNumero === 6 || penalNumero === 7) {
+    if (puntajes.penalNumero === 6 || puntajes.penalNumero === 7) {
         // Verificar si hay una diferencia de 3 goles en el marcador
         if (Math.abs(puntajes.jugadorPuntaje - puntajes.computadoraPuntaje) === 3) {
             setTimeout(() => { if (puntajes.jugadorPuntaje > puntajes.computadoraPuntaje) {
@@ -63,7 +62,7 @@ function playGame(jugadorEleccion) {
         }
     }
 
-    if (penalNumero === 8 ) {
+    if (puntajes.penalNumero === 8 ) {
         // Verificar si hay una diferencia de 2 goles en el marcador
         if (Math.abs(puntajes.jugadorPuntaje - puntajes.computadoraPuntaje) === 2) {
             setTimeout(() => {  if (puntajes.jugadorPuntaje > puntajes.computadoraPuntaje) {
@@ -77,7 +76,7 @@ function playGame(jugadorEleccion) {
         }
     }
 
-    if (penalNumero === 9) {
+    if (puntajes.penalNumero === 9) {
         // Verificar si hay una diferencia de 2 puntos en el marcador
         if (Math.abs(puntajes.jugadorPuntaje - puntajes.computadoraPuntaje) === 2 || puntajes.jugadorPuntaje - puntajes.computadoraPuntaje === -1) {
             setTimeout(() => {  if (puntajes.jugadorPuntaje > puntajes.computadoraPuntaje) {
@@ -91,7 +90,7 @@ function playGame(jugadorEleccion) {
         }
     }
 
-    if (penalNumero >= 10 && penalNumero % 2 === 0) {
+    if (puntajes.penalNumero >= 10 && puntajes.penalNumero % 2 === 0) {
         // Verificar si hay una diferencia de 1 punto en el marcador
         if (Math.abs(puntajes.jugadorPuntaje - puntajes.computadoraPuntaje) === 1) {
             setTimeout(() => {
@@ -105,7 +104,7 @@ function playGame(jugadorEleccion) {
             }, 100); // Retardo de 100 milisegundos
         } else {  
             setTimeout(() => {
-            penalNumero = 8; // Volver al turno 8
+            puntajes.penalNumero = 8; // Volver al turno 8
             document.querySelectorAll('.casillero img').forEach(img => img.remove());
         }, 500); // Retardo de 500 milisegundos
         }
@@ -122,12 +121,11 @@ function playGame(jugadorEleccion) {
 function reinicioJuego() {
     puntajes.jugadorPuntaje = 0;
     puntajes.computadoraPuntaje = 0; 
-    penalNumero = 0; 
+    puntajes.penalNumero = 0; 
     puntajes.pateadorTurno = true; 
 
     document.getElementById('jugador-puntaje').innerText = puntajes.jugadorPuntaje;
     document.getElementById('computadora-puntaje').innerText = puntajes.computadoraPuntaje;
-    document.getElementById('result').innerHTML = "¡El juego ha sido reiniciado!";
-    document.getElementById('tanda-de-penales').innerText = "1";
+    document.getElementById('result').innerHTML = "¡El juego ha finalizado!";
     document.querySelectorAll('.casillero img').forEach(img => img.remove());
 }
